5535d76f68e317d9f1036b092f960bc268e0a6dc
Ansel-Kee <a1.kee.jx@gmail.com>
2025-06-17 21:31:04 +0800

    cleanup

--- updates bl/Lifehack-2025 1/popup.js
+++
@@ -1,7 +0,0 @@
-
-console.log("Popup loaded");
-
-document.getElementById("leafBtn").addEventListener("click", () => {
-  const panel = document.getElementById("reportPanel");
-  panel.classList.toggle("hidden");
-});
